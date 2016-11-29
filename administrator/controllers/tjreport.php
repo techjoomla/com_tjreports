3b95ea38510722a7ed72446de456b15a4f8cd92c
Raviraj Powar <raviraj_p@tekditechnologies.com>
2016-11-29 12:12:16 +0530

    php cs

--- administrator/controllers/tjreport.php
+++ administrator/controllers/tjreport.php
@@ -23,7 +23,7 @@ class TjreportsControllerTjreport extends JControllerForm
 	 * Contructor
 	 */
 
-	function __construct()
+	public function __construct()
 	{
 		$this->view_list = 'tjreports';
 		parent::__construct();

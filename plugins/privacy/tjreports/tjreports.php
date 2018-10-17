e9b35fd70ef550b630c29f6e89acfc30e73ad37a
Pranoti Patil <pranoti_p@techjoomla.com>
2018-10-17 14:28:25 +0530

    Task #135591 feat: [GDPR Privacy Policy] - Add actionlog type of plugin for TJReports

---
+++ plugins/privacy/tjreports/tjreports.php
@@ -0,0 +1,163 @@
+<?php
+/**
+ * @package     Quick2Cart
+ * @subpackage  Plg_Privacy_Quick2Cart
+ *
+ * @author      Techjoomla <extensions@techjoomla.com>
+ * @copyright   Copyright (C) 2009 - 2018 Techjoomla. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('_JEXEC') or die();
+
+JLoader::register('PrivacyPlugin', JPATH_ADMINISTRATOR . '/components/com_privacy/helpers/plugin.php');
+JLoader::register('PrivacyRemovalStatus', JPATH_ADMINISTRATOR . '/components/com_privacy/helpers/removal/status.php');
+
+use Joomla\CMS\User\User;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\Model\BaseDatabaseModel;
+
+/**
+ * Quick2Cart Privacy Plugin.
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class PlgPrivacyTjreports extends PrivacyPlugin
+{
+	/**
+	 * Load the language file on instantiation.
+	 *
+	 * @var    boolean
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected $autoloadLanguage = true;
+
+	/**
+	 * Database object
+	 *
+	 * @var    JDatabaseDriver
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected $db;
+
+	/**
+	 * Reports the privacy related capabilities for this plugin to site administrators.
+	 *
+	 * @return  array
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function onPrivacyCollectAdminCapabilities()
+	{
+		$this->loadLanguage();
+
+		return array(
+			Text::_('PLG_PRIVACY_JTREPORTS') => array(
+				Text::_('PLG_PRIVACY_JTREPORTS_PRIVACY_CAPABILITY_USER_REPORTS_DETAIL')
+			)
+		);
+	}
+
+	/**
+	 * Processes an export request for TJReports user data
+	 *
+	 * This event will collect data for the following tables:
+	 *
+	 * - #__tj_reports
+	 *
+	 * @param   PrivacyTableRequest  $request  The request record being processed
+	 * @param   JUser                $user     The user account associated with this request if available
+	 *
+	 * @return  PrivacyExportDomain[]
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function onPrivacyExportRequest(PrivacyTableRequest $request, JUser $user = null)
+	{
+		if (!$user)
+		{
+			return array();
+		}
+
+		/** @var JTableUser $user */
+		$userTable = User::getTable();
+		$userTable->load($user->id);
+
+		$domains = array();
+		$domains[] = $this->createTJReportsUserReports($userTable);
+
+		return $domains;
+	}
+
+	/**
+	 * Create the domain for the TJReports user reports
+	 *
+	 * @param   JTableUser  $user  The JTableUser object to process
+	 *
+	 * @return  PrivacyExportDomain
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	private function createTJReportsUserReports(JTableUser $user)
+	{
+		$domain = $this->createDomain('User Reports', 'Reports of user in TJReports');
+
+		$query = $this->db->getQuery(true)
+			->select($this->db->quoteName(array('id', 'title', 'plugin', 'userid', 'client', 'default')))
+			->from($this->db->quoteName('#__tj_reports'))
+			->where($this->db->quoteName('userid') . '=' . $user->id);
+
+		$roles = $this->db->setQuery($query)->loadAssocList();
+
+		if (!empty($roles))
+		{
+			foreach ($roles as $role)
+			{
+				$domain->addItem($this->createItemFromArray($role, $role['id']));
+			}
+		}
+
+		return $domain;
+	}
+
+	/**
+	 * Removes the data associated with a remove information request
+	 *
+	 * This event will pseudoanonymise the user account
+	 *
+	 * @param   PrivacyTableRequest  $request  The request record being processed
+	 * @param   JUser                $user     The user account associated with this request if available
+	 *
+	 * @return  void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function onPrivacyRemoveData(PrivacyTableRequest $request, JUser $user = null)
+	{
+		// This plugin only processes data for registered user accounts
+		if (!$user)
+		{
+			return;
+		}
+
+		// If there was an error loading the user do nothing here
+		if ($user->guest)
+		{
+			return;
+		}
+
+		$db = $this->db;
+
+		// 1. Delete data from #__tj_reports
+		$query = $db->getQuery(true)
+			->delete($db->quoteName('#__tj_reports'))
+			->where($db->quoteName('userid') . '=' . $user->id)
+			->where($db->quoteName('default') . '=' . 0);
+
+		$db->setQuery($query);
+		$db->execute();
+	}
+}
---
+++ tjreports/script.tjreports.php
@@ -0,0 +1,168 @@
+<?php
+/**
+ * @version    SVN: <svn_id>
+ * @package    Com_Jticketing
+ * @copyright  Copyright (C) 2005 - 2017. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * Shika is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ */
+defined('_JEXEC') or die( ';)' );
+jimport('joomla.installer.installer');
+jimport('joomla.filesystem.file');
+jimport('joomla.application.component.helper');
+
+
+/**
+ * Script file of JTicketing component
+ *
+ * @since  1.0.0
+ **/
+class Com_TjreportsInstallerScript
+{
+/** @var array The list of extra modules and plugins to install */
+	private $installation_queue = array(
+
+		// plugins => { (folder) => { (element) => (published) }* }*
+		'plugins' => array(
+				'actionlog' => array(
+					'tjreports' => 1
+				),
+				'privacy' => array(
+					'tjreports' => 1
+				)
+			)
+		);
+
+	/**
+	 * This method is called after a component is installed.
+	 *
+	 * @param   \stdClass  $parent  Parent object calling this method.
+	 *
+	 * @return void
+	 */
+	public function install($parent)
+	{
+	}
+
+	/**
+	 * This method is called after a component is uninstalled.
+	 *
+	 * @param   \stdClass  $parent  Parent object calling this method.
+	 *
+	 * @return void
+	 */
+	public function uninstall($parent)
+	{
+	}
+
+	/**
+	 * This method is called after a component is updated.
+	 *
+	 * @param   \stdClass  $parent  Parent object calling object.
+	 *
+	 * @return void
+	 */
+	public function update($parent)
+	{
+	}
+
+	/**
+	 * Runs just before any installation action is preformed on the component.
+	 * Verifications and pre-requisites should run in this function.
+	 *
+	 * @param   string     $type    Type of PreFlight action. Possible values are:
+	 *                              - * install
+	 *                              - * update
+	 *                              - * discover_install
+	 * @param   \stdClass  $parent  Parent object calling object.
+	 *
+	 * @return void
+	 */
+	public function preflight($type, $parent)
+	{
+	}
+
+	/**
+	 * Runs right after any installation action is preformed on the component.
+	 *
+	 * @param   string     $type    Type of PostFlight action. Possible values are:
+	 *                             - * install
+	 *                             - * update
+	 *                             - * discover_install
+	 * @param   \stdClass  $parent  Parent object calling object.
+	 *
+	 * @return void
+	 */
+	public function postflight($type, $parent)
+	{
+		$src = $parent->getParent()->getPath('source');
+
+		$db = JFactory::getDbo();
+
+		$status = new JObject;
+		$status->plugins = array();
+
+		// Plugins installation
+		if (count($this->installation_queue['plugins']))
+		{
+			foreach ($this->installation_queue['plugins'] as $folder => $plugins)
+			{
+				if (count($plugins))
+				{
+					foreach ($plugins as $plugin => $published)
+					{
+						$path = "$src/plugins/$folder/$plugin";
+
+						if (!is_dir($path))
+						{
+							$path = "$src/plugins/$folder/plg_$plugin";
+						}
+
+						if (!is_dir($path))
+						{
+							$path = "$src/plugins/$plugin";
+						}
+
+						if (!is_dir($path))
+						{
+							$path = "$src/plugins/plg_$plugin";
+						}
+
+						if (!is_dir($path))
+						{
+							continue;
+						}
+
+						// Was the plugin already installed?
+						$query = $db->getQuery(true)
+							->select('COUNT(*)')
+							->from($db->qn('#__extensions'))
+							->where($db->qn('element') . ' = ' . $db->q($plugin))
+							->where($db->qn('folder') . ' = ' . $db->q($folder));
+						$db->setQuery($query);
+						$count = $db->loadResult();
+
+						$installer = new JInstaller;
+						$result = $installer->install($path);
+
+						$status->plugins[] = array('name' => 'plg_' . $plugin, 'group' => $folder, 'result' => $result);
+
+						if ($published && !$count)
+						{
+							$query = $db->getQuery(true)
+								->update($db->qn('#__extensions'))
+								->set($db->qn('enabled') . ' = ' . $db->q('1'))
+								->where($db->qn('element') . ' = ' . $db->q($plugin))
+								->where($db->qn('folder') . ' = ' . $db->q($folder));
+							$db->setQuery($query);
+							$db->execute();
+						}
+					}
+				}
+			}
+		}
+	}
+}

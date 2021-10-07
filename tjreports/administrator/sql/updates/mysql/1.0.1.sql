644bf5784fa16976a5763fae8264c0964c487cc3
Divya Chaudhari <divya_c@techjoomla.com>
2021-10-07 17:08:25 +0530

    Task #173952 Chore: Resolving Comments

--- tjreports/administrator/sql/updates/mysql/1.0.1.sql
+++ tjreports/administrator/sql/updates/mysql/1.0.1.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS `#__tj_reports` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `asset_id` int(11) NOT NULL DEFAULT '0',
+  `asset_id` int(11) NOT NULL DEFAULT 0,
   `title` varchar(255) NOT NULL DEFAULT '',
   `alias` varchar(255) NOT NULL DEFAULT '',
   `plugin` varchar(255) NOT NULL DEFAULT '',
   `client` varchar(255) NOT NULL DEFAULT '',
-  `parent` int(11) NOT NULL DEFAULT '0',
-  `default` tinyint(4) NOT NULL DEFAULT '0',
-  `userid` int(11) NOT NULL DEFAULT '0',
-  `datadenyset` int(11) DEFAULT '0',
+  `parent` int(11) NOT NULL DEFAULT 0,
+  `default` tinyint(4) NOT NULL DEFAULT 0,
+  `userid` int(11) NOT NULL DEFAULT 0,
+  `datadenyset` int(11) DEFAULT 0,
   `param` text NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) AUTO_INCREMENT=1 ;
--- tjreports/administrator/sql/updates/mysql/1.0.6.sql
+++ tjreports/administrator/sql/updates/mysql/1.0.6.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `#__tj_reports` add column `ordering` int(11) DEFAULT '0';
+ALTER TABLE `#__tj_reports` add column `ordering` int(11) DEFAULT 0;
